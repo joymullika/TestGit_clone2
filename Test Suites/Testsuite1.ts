433666eddcc360f63a9d09fb15fcf60507bba6dc
joymully <joymully@DESKTOP-83V0J6O>
2017-09-03 22:10:42 +0700

    Create Testsuite

---
+++ Test Suites/Testsuite1.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Testsuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2017-09-03T22:06:51</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>74e327f9-dc9e-4500-a74a-dccb54cf8e87</testSuiteGuid>
+   <testCaseLink>
+      <guid>0f81bf7b-c30c-480d-b883-d1d4a4674c9c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>839a0daf-5967-4588-bff1-221a0917cb4d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
